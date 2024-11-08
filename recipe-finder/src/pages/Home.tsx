a3561a2f510738edff3a758fd53998a7a054b25c
kkumber <angelbertreyes28@gmail.com>
2024-11-07 19:56:38 -0800

    Changed api to spoonacular

--- recipe-finder/src/pages/Home.tsx
+++ recipe-finder/src/pages/Home.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react"
 import useFetch from "../hooks/useFetch"
 
 const Home = () => {
-    const {data: recipe, loading, error} = useFetch('www.themealdb.com/api/json/v1/1/search.php?s=Chicken');
+    const APIKEY = 'ae98638f897c4eb79d6f212f141affb8';
+    const {data: recipe, loading, error} = useFetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2");
     console.log(recipe);
-
 
 
 

f2bb1c58b9cccc1b2c98cbec251d14fa7e36844d
kkumber <angelbertreyes28@gmail.com>
2024-11-11 18:55:14 -0800

    instructions is better than analyzedInstructions

--- recipe-finder/src/pages/Meal.tsx
+++ recipe-finder/src/pages/Meal.tsx
@@ -94,11 +94,12 @@ const Meal = () => {
 
                 <div className="instructionsContainer">
                     <h2>Instructions</h2>
-                    {instructions?.map((step, index) => 
+                    {/* {instructions?.map((step, index) => 
                         <ul className="stepsContainer" key={index}>
                             <li>{index + 1}. {step.step}</li>
                         </ul>
-                    )}
+                    )} */}{mealRecipe && <div className="instructions" dangerouslySetInnerHTML={{ __html: mealRecipe!.instructions} }></div>
+}
                 </div>
         </div>
      );

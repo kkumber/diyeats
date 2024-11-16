a092a9dee03a28483ede00fd9b0e6c3db9b02c8f
kkumber <angelbertreyes28@gmail.com>
2024-11-16 18:11:13 +0800

    Added delete function inside the useFavorites with re-renders

--- recipe-finder/src/hooks/useFavorites.tsx
+++ recipe-finder/src/hooks/useFavorites.tsx
@@ -5,17 +5,24 @@ import { useEffect, useState } from "react";
 const useFavorites = () => {
     const [favorites, setFavorites] = useState<ItemInterface[]>(useLocalStorage('favorites'));
 
-
     const isFavorite = (newItem: ItemInterface) => {
         const found = favorites.some(item => item.id === newItem.id);
         return found;
     }
 
     const addToFavorites = (newItem: ItemInterface) => {
-        localStorage.setItem('favorites', JSON.stringify([...favorites, newItem]));
+        const updatedList = [...favorites, newItem];
+        setFavorites(updatedList);
+        localStorage.setItem('favorites', JSON.stringify(updatedList));
     }
 
-    return {favorites, isFavorite, addToFavorites}
+    const removeToFavorites = (newItem: ItemInterface) => {
+        const updatedList = favorites.filter(item => item.id !== newItem.id);
+        setFavorites(updatedList);
+        localStorage.setItem('favorites', JSON.stringify(updatedList));
+    }
+
+    return {favorites, isFavorite, addToFavorites, removeToFavorites}
 }
 
 export default useFavorites;

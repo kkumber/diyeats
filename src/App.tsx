9b8211caa226d7389e76b450dbd0c8ce76384b80
kkumber <angelbertreyes28@gmail.com>
2024-11-20 13:20:10 +0800

    designed the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <Header />
       <Navbar />
+      <main>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/pages/Search' element={<Search />}/>
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import Home from "./Home";
 
+
 const Navbar = () => {
   return (
-  <nav>
-    <div className="navbarContainer">
-        <div className="homeContainer">
+  <nav className="mx-4 my-4 bg-dark-brown text-white rounded-full px-4 py-2 sticky inset-4">
+    <div className="flex justify-between items-center text-base sm:text-xl sm:px-24 md:px-40 md:text-xl">
             <Link to={`/`}>Home</Link>
             <Link to={`/pages/Favorites`}>Favorites</Link>
-        </div>
+            <Link to={`/pages/Explore`}>Explore</Link>
     </div>
   </nav>);
 };

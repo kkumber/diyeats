571cc2fdb317fa06ebab6052b1abd035db2b07db
kkumber <angelbertreyes28@gmail.com>
2024-11-21 21:41:25 +0800

    Added explore page

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,11 +1,19 @@
 import useFetch from "../hooks/useFetch";
+import RenderFoods from "../components/RenderFoods";
+import { useEffect, useState } from "react";
+import { ItemInterface } from "./Home";
 
 const Explore = () => {
-    const {data, error, loading} = useFetch(`https://api.spoonacular.com/recipes/random?`)
+    const APIKEY = process.env.REACT_APP_API_KEY;
+    const {data, error, loading} = useFetch(`https://api.spoonacular.com/recipes/random?apiKey=${APIKEY}&number=20`);
+    const [item, setItem] = useState<ItemInterface[]>([]);
 
+    useEffect(() => {
+        setItem(data);
+    }, [data])
     return ( 
         <div className="exploreContainer">
-            
+            <RenderFoods item={item} />
         </div>
      );
 }

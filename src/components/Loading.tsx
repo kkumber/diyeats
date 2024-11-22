176aebbc4cb42f38cc91ebdccb3cd72647108827
kkumber <angelbertreyes28@gmail.com>
2024-11-22 14:08:19 +0800

    Designed loading and error components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,14 @@
 const Loading = () => {
   return (
-    <div className="loadingContainer">
-        <h2>Loading...</h2>
-    </div>
+    <div className="flex flex-row gap-2 justify-center items-center">
+        <div className="w-4 h-4 rounded-full  bg-milky-brown animate-bounce"></div>
+        <div
+          className="w-4 h-4 rounded-full  bg-light-brown animate-bounce [animation-delay:-.3s]"
+        ></div>
+        <div
+          className="w-4 h-4 rounded-full bg-dark-brown animate-bounce [animation-delay:-.5s]"
+        ></div>
+      </div>
   );
 };
 
--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -2,18 +2,25 @@ import useFetch from "../hooks/useFetch";
 import RenderFoods from "../components/RenderFoods";
 import { useEffect, useState } from "react";
 import { ItemInterface } from "./Home";
+import Loading from "../components/Loading";
+import ErrorPage from "../components/ErrorPage";
 
 const Explore = () => {
     const APIKEY = process.env.REACT_APP_API_KEY;
-    const {data, error, loading} = useFetch(`https://api.spoonacular.com/recipes/random?apiKey=${APIKEY}&number=20`);
+    const {data, loading, error} = useFetch(`https://api.spoonacular.com/recipes/random?apiKey=${APIKEY}&number=20`);
     const [item, setItem] = useState<ItemInterface[]>([]);
 
     useEffect(() => {
         console.log(data);
-        setItem(data);
+        setItem(data.recipes);
     }, [data])
     return ( 
         <div className="exploreContainer">
+            {loading && <Loading />}
+            {error && <ErrorPage error={error} />}
+            <div className="mt-4 mb-2 sm:mt-12 sm:mb-4">
+               <span className="font-montserrat font-bold text-xl sm:text-3xl">Explore</span>
+            </div>
             <RenderFoods item={item} />
         </div>
      );

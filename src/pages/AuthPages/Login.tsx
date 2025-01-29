d3ef0a9ba7081dc5badffb82719b2b4ca0434b2e
kkumber <angelbertreyes28@gmail.com>
2025-01-29 23:07:40 +0800

    async and awaits

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -4,69 +4,81 @@ import ErrorPage from "../../components/ErrorPage";
 import Loading from "../../components/Loading";
 import { useNavigate } from "react-router-dom";
 
-
 const Login = () => {
-    const {data, isLoading, err, authFetch} = useAuthFetch();
-    const [userData, setUserData] = useState({username: '', password: ''});
-    const nav = useNavigate();
+  const { data, isLoading, err, authFetch } = useAuthFetch();
+  const [userData, setUserData] = useState({ username: "", password: "" });
+  const nav = useNavigate();
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (userData) {
-            authFetch('accounts/auth/login/', userData);
-        }
-        nav('/');
-    };
-
-    const handleChangeuserData = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setUserData({
-            ...userData,
-            [name]: value
-        });
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (userData) {
+      await authFetch("accounts/auth/login/", userData);
     }
+    nav("/");
+  };
 
-    const handleRedirect = () => {
-        nav('/register');
-    }
+  const handleChangeuserData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
 
-    return ( 
-        <div className="flex justify-center items-center transition-all duration-500 ease-in-out my-12">    
-            <form onSubmit={handleSubmit}>
-                <h2 className="text-5xl mb-8 text-center"><b>Login</b></h2>
-                
-                {/* Container for the inputs */}
-                <div className="bg-light-brown p-12 rounded-md text-xl">
-                    <label htmlFor="username">Enter Username: </label> <br />
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        required={true}
-                        value={userData.username}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
-                    /> <br /> <br />
-                    <label htmlFor="password">Enter Password: </label> <br />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        required={true}
-                        value={userData.password}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2"
-                    /> <br /> <br /> <br />
+  const handleRedirect = () => {
+    nav("/register");
+  };
 
-                    {/* Button container */}
-                    <div className="flex flex-wrap justify-between">
-                        <button type="submit" className="bg-dark-brown py-2 px-4 rounded-full text-white hover:-translate-y-1 ease-in-out" >Sign in</button>
-                        <button onClick={() => handleRedirect()} className="py-2 px-4 rounded-full text-light-brown bg-white hover:-translate-y-1 ease-in-out">Register</button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="flex justify-center items-center transition-all duration-500 ease-in-out my-12">
+      <form onSubmit={handleSubmit}>
+        <h2 className="text-5xl mb-8 text-center">
+          <b>Login</b>
+        </h2>
+
+        {/* Container for the inputs */}
+        <div className="bg-light-brown p-12 rounded-md text-xl">
+          <label htmlFor="username">Enter Username: </label> <br />
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            required={true}
+            value={userData.username}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
+          />{" "}
+          <br /> <br />
+          <label htmlFor="password">Enter Password: </label> <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required={true}
+            value={userData.password}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2"
+          />{" "}
+          <br /> <br /> <br />
+          {/* Button container */}
+          <div className="flex flex-wrap justify-between">
+            <button
+              type="submit"
+              className="bg-dark-brown py-2 px-4 rounded-full text-white hover:-translate-y-1 ease-in-out"
+            >
+              Sign in
+            </button>
+            <button
+              onClick={() => handleRedirect()}
+              className="py-2 px-4 rounded-full text-light-brown bg-white hover:-translate-y-1 ease-in-out"
+            >
+              Register
+            </button>
+          </div>
         </div>
-     );
-}
- 
+      </form>
+    </div>
+  );
+};
+
 export default Login;
--- src/pages/AuthPages/Register.tsx
+++ src/pages/AuthPages/Register.tsx
@@ -5,91 +5,109 @@ import Loading from "../../components/Loading";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const {data, isLoading, err, authFetch} = useAuthFetch();
-    const [userData, setUserData] = useState({username: '', password: '', email: '', confirm_password: ''})
-    const nav = useNavigate();
+  const { data, isLoading, err, authFetch } = useAuthFetch();
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    confirm_password: "",
+  });
+  const nav = useNavigate();
 
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (userData) {
-            authFetch('accounts/auth/register/', userData);
-        }
-        nav('/login');
-    };
-
-    const handleChangeuserData = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setUserData({
-            ...userData,
-            [name]: value
-        }); 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (userData) {
+      await authFetch("accounts/auth/register/", userData);
     }
+    nav("/login");
+  };
 
-    const handleRedirect = () => {
-        nav('/login');
-    }
+  const handleChangeuserData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
 
-    return ( 
-        <div className="flex justify-center items-center transition-all duration-500 ease-in-out my-12">    
-            
-        <form onSubmit={handleSubmit}>
-            {isLoading && <Loading />}
-            {err && <ErrorPage error={err} />}
-    
-            <h2 className="text-5xl mb-8 text-center"><b>Register</b></h2>
-            
-            {/* Container for inputs */}
-            <div className="bg-light-brown p-12 rounded-md text-xl">
-            <label htmlFor="username">Enter Username: </label> <br />
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        required={true}
-                        value={userData.username}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
-                    /> <br /> <br />
-                    <label htmlFor="email">Enter Email: </label> <br />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={userData.email}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
-                    /> <br /> <br />
-                    <label htmlFor="password">Enter Password: </label> <br />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        required={true}
-                        value={userData.password}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
-                    /> <br /> <br />
-                    <label htmlFor="confirm_password">Confirm Password: </label> <br />
-                    <input
-                        type="password"
-                        name="confirm_password"
-                        placeholder="Confirm Password"
-                        required={true}
-                        value={userData.confirm_password}
-                        onChange={handleChangeuserData}
-                        className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
-                    /> <br /> <br /> <br />
-                {/* Buttons Container */}
-                <div className="flex flex-wrap justify-between">
-                    <button type="submit" className="bg-dark-brown py-2 px-4 rounded-full text-white hover:-translate-y-1 ease-in-out" >Sign up</button>
-                    <button onClick={() => handleRedirect()} className="py-2 px-4 rounded-full text-light-brown bg-white hover:-translate-y-1 ease-in-out">Login</button>
-                </div>
-            </div>
-        </form>
+  const handleRedirect = () => {
+    nav("/login");
+  };
+
+  return (
+    <div className="flex justify-center items-center transition-all duration-500 ease-in-out my-12">
+      <form onSubmit={handleSubmit}>
+        {isLoading && <Loading />}
+        {err && <ErrorPage error={err} />}
+
+        <h2 className="text-5xl mb-8 text-center">
+          <b>Register</b>
+        </h2>
+
+        {/* Container for inputs */}
+        <div className="bg-light-brown p-12 rounded-md text-xl">
+          <label htmlFor="username">Enter Username: </label> <br />
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            required={true}
+            value={userData.username}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
+          />{" "}
+          <br /> <br />
+          <label htmlFor="email">Enter Email: </label> <br />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={userData.email}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
+          />{" "}
+          <br /> <br />
+          <label htmlFor="password">Enter Password: </label> <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required={true}
+            value={userData.password}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
+          />{" "}
+          <br /> <br />
+          <label htmlFor="confirm_password">Confirm Password: </label> <br />
+          <input
+            type="password"
+            name="confirm_password"
+            placeholder="Confirm Password"
+            required={true}
+            value={userData.confirm_password}
+            onChange={handleChangeuserData}
+            className="focus:outline-none rounded-sm bg-transparent border-b-2 py-2"
+          />{" "}
+          <br /> <br /> <br />
+          {/* Buttons Container */}
+          <div className="flex flex-wrap justify-between">
+            <button
+              type="submit"
+              className="bg-dark-brown py-2 px-4 rounded-full text-white hover:-translate-y-1 ease-in-out"
+            >
+              Sign up
+            </button>
+            <button
+              onClick={() => handleRedirect()}
+              className="py-2 px-4 rounded-full text-light-brown bg-white hover:-translate-y-1 ease-in-out"
+            >
+              Login
+            </button>
+          </div>
         </div>
+      </form>
+    </div>
+  );
+};
 
-     );
-}
- 
 export default Register;
